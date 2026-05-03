0b2d4af3d85dd150d18d05faba14c9b89a57e606
lucas794 <lgtorres@fi.uba.ar>
2026-05-03 12:50:33 -0300

    Now added tests

--- tests/health.rs
+++ tests/health.rs
@@ -1,10 +1,22 @@
 use axum::{body::Body, http::Request, http::StatusCode};
+use dotenvy::dotenv;
 use http_body_util::BodyExt;
 use tower::ServiceExt;
 
+async fn create_basic_pool() -> sqlx::PgPool {
+    // set enviroment variables
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    sqlx::PgPool::connect(&database_url)
+        .await
+        .expect("Failed to connect to the database")
+}
+
 #[tokio::test]
 async fn health_returns_ok() {
-    let app = backend_website::app();
+    let pool = create_basic_pool().await;
+    let app = backend_website::app(pool);
 
     let response = app
         .oneshot(
@@ -24,7 +36,8 @@ async fn health_returns_ok() {
 }
 #[tokio::test]
 async fn hello_returns_ok() {
-    let app = backend_website::app();
+    let pool = create_basic_pool().await;
+    let app = backend_website::app(pool);
 
     let response = app
         .oneshot(
