a7dc39bde5aedeff1f07fbe12534c99cb55f9315
Federico Valsagna <fvalsagna@fi.uba.ar>
2026-06-11 03:02:18 -0300

    Moving the shares to user shares

--- src/endpoints/mod.rs
+++ src/endpoints/mod.rs
@@ -2,3 +2,4 @@ pub mod health;
 pub mod hello;
 pub mod share;
 pub mod user;
+pub mod user_share;
--- src/endpoints/share/mod.rs
+++ src/endpoints/share/mod.rs
@@ -1,5 +1,4 @@
-pub mod delete_logic;
-pub mod get_logic;
-pub mod post_logic;
-pub mod put_logic;
+// pub mod delete_logic;
+// pub mod post_logic;
+// pub mod put_logic;
 pub mod get_all_logic;
